8c7ee3034d0267edded51ee81a5b878c12e56ef4
Arnau Soguero Agustí <arnau5_93@hotmail.com>
2018-02-15 10:59:01 +0100

    Aún no es online

---
+++ PingPong/PingPong/Juego.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PingPong
+{
+    partial class Juego
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox2.Location = new System.Drawing.Point(555, 414);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 73);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(95, 107);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Juego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(565, 499);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Juego";
+            this.Text = "Ping pong";
+            this.Load += new System.EventHandler(this.Juego_Load);
+            this.Click += new System.EventHandler(this.Juego_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Juego_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ PingPong/PingPong/Juego.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace PingPong
+{
+    public partial class Juego : Form
+    {
+        public Juego()
+        {
+            InitializeComponent();
+        }
+
+        int velocidad = 5;
+        int cont = 0;
+        int puntuaje = 0;
+
+        bool Arriba, Izquierda;
+
+       
+        private void Juego_MouseMove(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Top= e.Y;
+        }
+
+        private void Juego_Load(object sender, EventArgs e)
+        {
+            this.Text = "Puntuaje: 0";
+            Random aleatorio = new Random();
+            pictureBox1.Location = new Point(0, aleatorio.Next(this.Height));
+            Arriba = true;
+            Izquierda = true;
+            timer1.Enabled = true;
+            puntuaje = 0;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Left > pictureBox2.Left) //perdemos
+            { timer1.Enabled = false;
+            MessageBox.Show("Puntuaje: " + puntuaje.ToString() + " puntos");
+            puntuaje = 0;
+            velocidad = 5;
+            cont = 0;
+
+            }
+
+            if (pictureBox1.Left + pictureBox1.Width >= pictureBox2.Left && pictureBox1.Left + pictureBox1.Width <= pictureBox2.Left + pictureBox2.Width
+                &&
+                pictureBox1.Top + pictureBox1.Height >= pictureBox2.Top &&
+                pictureBox1.Top + pictureBox1.Height <= pictureBox2.Top + pictureBox2.Height)
+            {
+                Izquierda = false;
+                puntuaje += 1;
+                this.Text = "Puntuacion: " + puntuaje.ToString() + " puntos";
+                cont += 1;
+                if (cont > 5)
+                { velocidad += 1;
+                cont = 0;
+                }
+            
+            }
+
+
+
+            #region Movimiento de la pelota
+            if (Izquierda)
+            { pictureBox1.Left += velocidad; }
+            else
+            { pictureBox1.Left -= velocidad; }
+            if (Arriba)
+            { pictureBox1.Top += velocidad;}
+            else
+            { pictureBox1.Top -= velocidad; }
+
+            if (pictureBox1.Top >= this.Height -50)//si pega en la pared de abajo
+            {
+                Arriba = false;
+            
+            }
+
+            if (pictureBox1.Top <= 0)
+            {
+                Arriba = true;
+            
+            }
+            if (pictureBox1.Left <= 0)
+            {
+                Izquierda = true;
+            }
+             if (pictureBox1.Left >= this.Width-10)
+            {
+                Izquierda = false;
+            }
+
+#endregion
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
